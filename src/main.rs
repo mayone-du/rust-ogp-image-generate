1df146c44145779a46d790e2461b678d95d9d60a
mayone-du <cocomayo1201@icloud.com>
2021-10-01 16:38:53 +0900

    Add sample codes by zenn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use image::Rgba;
+use imageproc::drawing::draw_text_mut;
+use rusttype::{Font, Scale};
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = match image::open("assets/images/220x220.jpg") {
+        Ok(image) => image,
+        Err(err) => panic!("画像を読み取れませんでした。{:?}", err),
+    };
+    let font = Vec::from(include_bytes!("../assets/fonts/azuki.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+
+    let size = 10.0;
+    let scale = Scale { x: size, y: size };
+
+    let text = "コマンドライン引数の文字列が入ります";
+    draw_text_mut(
+        &mut image,
+        Rgba([0u8, 0u8, 0u8, 0u8]),
+        10,
+        0,
+        scale,
+        &font,
+        text,
+    );
+
+    image.save("test.jpg").unwrap();
 }
